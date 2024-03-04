09012260430212ff60700c1bd2c1ca49ffd909f9
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2024-03-04 15:18:50 +0100

    klar med javascript och kundvagn

---
+++ script.js
@@ -0,0 +1,23 @@
+let cart = [];
+
+function additem(itemn, itemp) {
+    let item = {
+        vara: itemn,
+        pris: itemp
+    };
+    cart.push(item);
+    varukorg();
+}
+function varukorg() {
+    let cartE = document.getElementById('cart');
+    cartE.innerHTML = '';
+    cart.forEach(function(item) {
+        let itemN = document.createElement('h4');
+        let itemP = document.createElement('p');
+        itemN.textContent = item.vara;
+        itemP.textContent = '$' + item.pris;
+        cartE.appendChild(itemN);
+        cartE.appendChild(itemP);
+    });
+}   
+
